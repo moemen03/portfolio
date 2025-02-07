bf4df360bd07d23bca048e1e1d2d5b7808f4eb44
moemen03 <moemenatia3@gmail.com>
2025-02-07 08:24:06 +0200

    under coding

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Navbar from "./sections/Navbar";
 export default function App() {
   return (
     <main className="max-w-7xl mx-auto relative">
+      <div className="nav-li fixed top-[80px] left-0 z-50 rounded-xl bg-[#00012A] p-5 text-white text-center">
+        still under coding
+      </div>
       <Navbar/>
       <Hero/>
       <About/>
