a733582433e40703f4b8d2ff9bb468a42d4c655f
moemen03 <moemenatia3@gmail.com>
2025-05-04 15:42:03 +0300

    akher marra

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,6 @@ import Tech from "./sections/Tech";
 export default function App() {
   return (
     <main className="max-w-7xl mx-auto relative">
-      <div className="nav-li fixed top-[80px] left-0 z-50 rounded-xl bg-[#00012A] p-5 text-white text-center">
-        still under coding
-      </div>
       <Navbar/>
       <Hero/>
       <About/>
