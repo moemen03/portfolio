987348305ffe9fcd65641aa550b75fe2fc967033
Moamen ALaa <89150107+moemen03@users.noreply.github.com>
2026-06-18 15:17:42 +0300

    fix: remove instructor title from hero section text

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
           Hi, I am Moamen <span className="waving-hand">👋</span>
         </p>
-        <p className="hero_tag text-gray_gradient">Software Developer & Instructor</p>
+        <p className="hero_tag text-gray_gradient">Software Developer </p>
       </div>
 
       <div className="w-full h-full absolute inset-0">
